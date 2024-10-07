0ffa91d8cfb5023c6112e564b70fc058e36afedf
Alisson <alissonfidelis147@gmail.com>
2024-10-07 20:57:20 -0300

    Finalizado tudo 2.0

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Obrigado. Eu retornarei para você assim que possível.");
 
           setForm({
             name: "",
@@ -59,7 +59,7 @@ const Contact = () => {
           setLoading(false);
           console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert("Ahh, Algo deu errado. Por favor, tente novamente.");
         }
       );
   };
